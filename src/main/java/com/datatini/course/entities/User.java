1c9a5f012fbcd9daa61fc4e4fc106858cb438cd0
Faustino Alfredo <faustinoalfredo06@gmail.com>
2024-03-24 18:06:04 +0100

    Order, Instant, Iso 8601 - Part 2

--- src/main/java/com/datatini/course/entities/User.java
+++ src/main/java/com/datatini/course/entities/User.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,14 +20,16 @@ public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
-	@OneToMany(mappedBy = "client")
+	@JsonIgnore
+	@OneToMany(mappedBy = "client") 
 	private List<Order> orders = new ArrayList<>();
 	
 	
---
+++ src/main/java/com/datatini/course/repositories/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.datatini.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.datatini.course.entities.Order;
+
+
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+
+}
