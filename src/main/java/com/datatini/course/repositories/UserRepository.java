797b9dac4fb5c0985308c686e9b690b93dc4d386
Faustino Alfredo <faustinoalfredo06@gmail.com>
2024-03-24 14:25:05 +0100

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/datatini/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.datatini.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.datatini.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+
+}
